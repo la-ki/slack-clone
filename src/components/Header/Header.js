9715c775d58589a54fb7a83e60ce88f9a5f61bf8
la-ki <layarkostic94@gmail.com>
2020-09-01 10:53:56 +0200

    Added Header component and styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Header.css';
 
 import {Avatar} from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
     return (
@@ -9,15 +12,19 @@ function Header() {
             <div className="header__left">
                 <Avatar 
                     className="header__avatar"
-                    alt={user?.displayName}
-                    src={user?.photoUrl}
+                    alt="Lazar Kostic"
+                    src="https://scontent.fbeg7-1.fna.fbcdn.net/v/t1.0-9/35159270_1885118164861091_6967089570097659904_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=cJQrJjzklzYAX_P0Dyc&_nc_ht=scontent.fbeg7-1.fna&oh=714f09a79673c2c923b2bea66e52039d&oe=5F737D20"
+                    //alt={user?.displayName}
+                    //src={user?.photoUrl}
                 />
+                <AccessTimeIcon />
             </div>
             <div className="header__search">
-
+                <SearchIcon />
+                <input placeholder="Search Lazar Kostic" type="text"/>
             </div>
             <div className="header__right">
-
+                <HelpOutlineIcon />
             </div>
         </div>
     )
